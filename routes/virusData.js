2a0467a230f403cf94ee7e0802a64b83f354e3ad
Chen <codingme@icloud.com>
2022-06-11 17:24:48 +0800

    feat: 增加获取gov公告接口

--- routes/virusData.js
+++ routes/virusData.js
@@ -8,6 +8,21 @@ router.get('/trend', async (req, res) => {
   const rowData = await collection2
     .where({
       now: _.gt(100000),
+      category: _.eq('baidu'),
+    })
+    .orderBy('now', 'desc')
+    .limit(1)
+    .get()
+
+  res.send(rowData.data[0])
+})
+
+//获取gov公告
+
+router.get('/announcement', async (req, res) => {
+  const rowData = await collection2
+    .where({
+      category: _.eq('gov'),
     })
     .orderBy('now', 'desc')
     .limit(1)
